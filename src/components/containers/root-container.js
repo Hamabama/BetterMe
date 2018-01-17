7622f67f94f26266cf99e0a489f0d1ff8b94efa5
Hamabama <yastr.dima@gmail.com>
2018-01-17 13:29:56 +0500

    add root container

---
+++ src/components/containers/root-container.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Header } from 'react-native-elements';
+import colors from '../../constants/colors';
+
+import Navigator from '../navigation/navigator';
+
+class RootContainer extends React.Component {
+
+    onAddPress() {
+        this.props.navigation.navigate('Add');
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Header
+                    centerComponent={{ text: 'BETTERME', style: styles.headerCenterComponent }}
+                    outerContainerStyles={styles.headerOuterContainer}
+                />
+                <Navigator />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.CHARCOAL
+    },
+    headerCenterComponent: {
+        color: '#fff',
+        fontSize: 20
+    },
+    headerOuterContainer: {
+        marginTop: 24,
+        borderBottomWidth: 0
+    }
+});
+
+export default RootContainer;
