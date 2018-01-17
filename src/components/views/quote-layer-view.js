4503d6e1857df0d58718267513956d9dfa554293
Hamabama <yastr.dima@gmail.com>
2018-01-17 13:23:23 +0500

    add QuoteLayerView

---
+++ src/components/views/quote-layer-view.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import colors from '../../constants/colors';
+import QuoteFormContainer from '../containers/quote-form-container';
+
+const QuoteLayerView = (props) => {
+    return (
+        <View style={styles.container}>
+            <QuoteFormContainer navigation={props.navigation} />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.CHARCOAL
+    }
+});
+
+export default QuoteLayerView;
