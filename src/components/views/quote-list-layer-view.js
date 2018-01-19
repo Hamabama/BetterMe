c2bc90e06f47c69b77c2b26758271a3cbded7f4b
Hamabama <yastr.dima@gmail.com>
2018-01-19 14:28:26 +0500

    add navigate call to show quote card

--- src/components/views/quote-list-layer-view.js
+++ src/components/views/quote-list-layer-view.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { View, ScrollView, StyleSheet } from 'react-native';
 import { Icon } from 'react-native-elements';
 import colors from '../../constants/colors';
+import store from '../../reducers/store';
 import QuoteListContainer from '../containers/quote-list-container';
 
 
 class QuoteListLayerView extends React.Component {
 
+    componentDidMount() {
+        this.unsubscribe = store.subscribe(() => {
+            const state = store.getState();
+            this.props.navigation.navigate('Current', { id: state.currentQuoteId });
+        })
+    }
     onAddPress() {
         this.props.navigation.navigate('Add');
     }
@@ -17,7 +24,11 @@ class QuoteListLayerView extends React.Component {
                     <QuoteListContainer />
                 </ScrollView>
                 <View style={styles.footer} >
-                    <Icon name='add' size={30} raised onPress={this.onAddPress.bind(this)} />
+                    <Icon name='add'
+                        size={26}
+                        raised
+                        onPress={this.onAddPress.bind(this)}
+                        containerStyle={styles.addIcon} />
                 </View>
             </View>
         );
@@ -34,6 +45,9 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: colors.BACKGROUND,
         marginTop: 0
+    },
+    addIcon: {
+        borderRadius: 3
     }
 });
 
