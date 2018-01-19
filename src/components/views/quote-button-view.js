c3225ea739ea433b4f0588839162dd957ae99349
Hamabama <yastr.dima@gmail.com>
2018-01-19 15:29:37 +0500

    add stylesheet

--- src/components/views/quote-button-view.js
+++ src/components/views/quote-button-view.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import { Button, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Button } from 'react-native-elements';
 
 const QuoteButtonView = (props) => {
     return (
-        <View style={{ flex: 1, margin: 10 }}>
-            <Button onPress={props.onClick} title={props.title}
-                color={props.color} />
+        <View style={styles.container}>
+            <Button onPress={props.onClick}
+                title={props.title}
+                buttonStyle={styles.button}
+                textStyle={styles.buttonText}
+                borderRadius={styles.button.borderRadius}
+            />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 0
+    },
+    button: {
+        backgroundColor: '#fff',
+        borderRadius: 3
+    },
+    buttonText: {
+        color: '#000'
+    }
+});
 
 export default QuoteButtonView;
