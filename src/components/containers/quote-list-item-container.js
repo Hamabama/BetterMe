a72297dd76adc610727e2420c426150a09ad24bf
Hamabama <yastr.dima@gmail.com>
2018-01-19 14:46:59 +0500

    add SET_CURRENT_QUOTE dispatch, assign background color of item list based on reminder time, quote removal

--- src/components/containers/quote-list-item-container.js
+++ src/components/containers/quote-list-item-container.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import QuoteListItemView from '../views/quote-list-item-view';
 import store from '../../reducers/store';
+import colors from '../../constants/colors';
 
 class QuoteListItemContainer extends React.Component {
 
     constructor() {
         super();
         this.state = {
-            showDeleteButton: false
+            showDeleteButton: false,
+            backgroundColor: '#3D6DCC'
         }
+
+    }
+    componentDidMount() {
+        this.getBackgroundColor();
+    }
+
+    getBackgroundColor() {
+        const backgroundColor = colors[this.props.quote.time];
+        this.setState({ backgroundColor });
     }
 
     onLongPress() {
@@ -16,20 +27,25 @@ class QuoteListItemContainer extends React.Component {
     }
 
     onPress() {
-        this.setState({ showDeleteButton: false });
+        if (this.state.showDeleteButton === true) {
+            this.setState({ showDeleteButton: false });
+            return;
+        }
+        store.dispatch({ type: 'SET_CURRENT_QUOTE', id: this.props.quote.id });
     }
 
     onPressRightIcon() {
-       store.dispatch({ type: 'REMOVE_QUOTE', id: this.props.quote.id });
+        store.dispatch({ type: 'REMOVE_QUOTE', id: this.props.quote.id });
     }
 
     render() {
-        return <QuoteListItemView 
-        quote={this.props.quote} 
-        onLongPress={this.onLongPress.bind(this)}
-        onPress={this.onPress.bind(this)}
-        showDeleteButton={this.state.showDeleteButton}
-        onPressRightIcon={this.onPressRightIcon.bind(this)}
+        return <QuoteListItemView
+            quote={this.props.quote}
+            onLongPress={this.onLongPress.bind(this)}
+            onPress={this.onPress.bind(this)}
+            showDeleteButton={this.state.showDeleteButton}
+            onPressRightIcon={this.onPressRightIcon.bind(this)}
+            backgroundColor={this.state.backgroundColor}
         />;
     }
 }
