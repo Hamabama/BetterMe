0e60e9471ca44f3bac6fac02bd51f197096ac4fc
Hamabama <yastr.dima@gmail.com>
2018-01-19 15:41:11 +0500

    add QuoteCardContainer logic

---
+++ src/components/containers/quote-card-container.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import QuoteCardView from '../views/quote-card-view';
+import store from '../../reducers/store';
+
+class QuoteCardContainer extends React.Component {
+
+    getCurrentCard(id) {
+        const state = store.getState();
+        const list = state.quoteList;
+        const currentList = list.filter(quote => quote.id === id);
+        return currentList[0];
+    }
+
+    render() {
+        const { state } = this.props.navigation;
+        const currentCard = this.getCurrentCard(state.params.id);
+        return <QuoteCardView {...currentCard} />;
+    }
+}
+
+export default QuoteCardContainer;
