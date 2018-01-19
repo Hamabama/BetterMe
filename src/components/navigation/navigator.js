9e937b95795a997898ee629a897cf03f48297e73
Hamabama <yastr.dima@gmail.com>
2018-01-19 14:55:55 +0500

    add QuoteCardScreen for navigation

--- src/components/navigation/navigator.js
+++ src/components/navigation/navigator.js
@@ -2,6 +2,7 @@ import { StackNavigator } from 'react-navigation';
 import React from 'react';
 import QuoteListLayerView from '../views/quote-list-layer-view';
 import QuoteLayerView from '../views/quote-layer-view';
+import QuoteCardContainer from '../containers/quote-card-container';
 
 const QuoteListScreen = (props) => {
     return <QuoteListLayerView navigation={props.navigation} />;
@@ -11,18 +12,20 @@ const AddQuoteScreen = (props) => {
     return <QuoteLayerView navigation={props.navigation} />;
 }
 
+const ShowCurrentQuoteScreen = (props) => {
+    return <QuoteCardContainer navigation={props.navigation} />;
+}
+
 const Navigator = StackNavigator({
     Main: {
-        screen: QuoteListScreen,
-        navigationOptions: {
-            headerTitle: 'List'
-        }
+        screen: QuoteListScreen
+
     },
     Add: {
-        screen: AddQuoteScreen,
-        navigationOptions: {
-            headerTitle: 'Add'
-        }
+        screen: AddQuoteScreen
+    },
+    Current: {
+        screen: ShowCurrentQuoteScreen
     }
 }, { headerMode: 'none' });
 
