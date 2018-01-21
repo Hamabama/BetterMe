0c6cd4dcd6c998eb723510639f1a9c3368bb7be0
Hamabama <yastr.dima@gmail.com>
2018-01-21 13:47:01 +0500

    updated tests

--- src/reducers/quote-list-reducer.test.js
+++ src/reducers/quote-list-reducer.test.js
@@ -1,5 +1,5 @@
 import quoteList from './quote-list-reducer';
-import time from '../constants/time';
+import store from '../reducers/store';
 
 describe('quoteList reducer', () => {
 
@@ -13,7 +13,9 @@ describe('quoteList reducer', () => {
 
     it('should return non-empty array on ADD_QUOTE on initial state', () => {
 
-        const action = { type: 'ADD_QUOTE', text: 'test', time: time.DAY };
+        const state = store.getState();
+
+        const action = { type: 'ADD_QUOTE', text: 'test', time: state.reminderTime.DAY };
         const stateAfter = 1;
 
         expect(quoteList(undefined, action).length).toEqual(stateAfter);
@@ -30,10 +32,10 @@ describe('quoteList reducer', () => {
 
     });
 
-    it('should return the same size array on CHANGE_TIME action', () => {
+    it('should return the same size array on CHANGE_QUOTE_REMINDER_TIME action', () => {
 
         const stateBefore = [{}, {}];
-        const action = { type: 'CHANGE_TIME' };
+        const action = { type: 'CHANGE_QUOTE_REMINDER_TIME' };
         const stateAfter = [{}, {}];
 
         expect(quoteList(stateBefore, action)).toEqual(stateAfter);
@@ -41,7 +43,10 @@ describe('quoteList reducer', () => {
     });
 
     it('should remove the quote with certain id from the array', () => {
-        const quote = { id: 0, text: 'test', time: time.DAY };
+
+        const state = store.getState();
+
+        const quote = { id: 0, text: 'test', time: state.reminderTime.DAY };
         const stateBefore = [quote, { ...quote, id: 1 }, { ...quote, id: 2 }];
         const action = { type: 'REMOVE_QUOTE', id: 1 };
         const stateAfter = [quote, { ...quote, id: 2 }];
