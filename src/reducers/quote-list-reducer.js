4723b244c4ee03a16ca2956596028b52ecf9ba19
Hamabama <yastr.dima@gmail.com>
2018-01-21 13:45:42 +0500

    renamed CHANGE_TIME action

--- src/reducers/quote-list-reducer.js
+++ src/reducers/quote-list-reducer.js
@@ -4,10 +4,10 @@ const quoteList = (state = [], action) => {
     switch (action.type) {
         case 'ADD_QUOTE':
             return [...state, quote(undefined, action)];
-        case 'CHANGE_TIME':
-            return state.map(q => quote( q, action));
+        case 'CHANGE_QUOTE_REMINDER_TIME':
+            return state.map(q => quote(q, action));
         case 'REMOVE_QUOTE':
-        return state.filter(q => q.id !== action.id);
+            return state.filter(q => q.id !== action.id);
         default: return state;
     }
 }
