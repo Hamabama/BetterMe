efb0d0b2f3501191666cb4f24ad38100b4b0fae2
Hamabama <yastr.dima@gmail.com>
2018-01-22 14:38:45 +0500

    removed store subscribtion

--- src/components/containers/quote-list-container.js
+++ src/components/containers/quote-list-container.js
@@ -2,20 +2,9 @@ import React from 'react';
 import store from '../../reducers/store';
 import QuoteListView from '../views/quote-list-view';
 
-class QuoteListContainer extends React.Component {
-    componentDidMount() {
-        this.unsubscribe = store.subscribe(() => {
-            this.forceUpdate();
-        });
-    }
-    componentWillUnmount() {
-        this.unsubscribe();
-    }
-
-    render() {
-        const state = store.getState();
-        return <QuoteListView quoteList={state.quoteList} />;
-    }
+const QuoteListContainer = (props) => {
+    const state = store.getState();
+    return <QuoteListView quoteList={state.quoteList} navigation={props.navigation} />;
 }
 
 export default QuoteListContainer;
