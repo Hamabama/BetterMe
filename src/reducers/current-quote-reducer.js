db57f76c71ac14c5345223b2d8879aaff6525cf8
Hamabama <yastr.dima@gmail.com>
2018-01-22 14:56:13 +0500

    no longer used currentQuoteReducer

--- src/reducers/current-quote-reducer.js
+++
@@ -1,9 +0,0 @@
-const currentQuoteId = (state = null, action) => {
-    switch (action.type) {
-        case 'SET_CURRENT_QUOTE':
-            return action.id;
-        default: return state;
-    }
-}
-
-export default currentQuoteId;
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,10 +1,8 @@
 import quoteList from './quote-list-reducer';
-import currentQuoteId from './current-quote-reducer';
 import reminders from './reminders-reducer';
 import { combineReducers } from 'redux';
 
 export default combineReducers({
     quoteList,
-    currentQuoteId,
     reminders
 });
