19059581dffe33e33bf4e0963650fad4bb0eac29
Hamabama <yastr.dima@gmail.com>
2018-01-25 18:57:36 +0500

    formatting

--- src/components/helpers/time.js
+++ src/components/helpers/time.js
@@ -1,17 +1,38 @@
-const getEpochTime = ({ hours, minutes }) => {
-    const time = new Date();
-    time.setHours(hours, minutes);
-    return time;
+const getEpochTime = (time) => {
+
+    const scheduleTime = new Date();
+
+    const timeObj = breakTimeString(time);
+
+    scheduleTime.setHours(timeObj.hours, timeObj.minutes);
+
+    return scheduleTime;
+}
+
+const breakTimeString = (time) => {
+
+    const timeArray = time.split(':');
+
+    return {
+        hours: timeArray[0],
+        minutes: timeArray[1]
+    }
 }
 
 const getCurrentTimeName = () => {
+
     const date = new Date();
+
     const hours = date.getHours();
 
     if (hours >= 6 && hours <= 12) {
+
         return 'MORNING';
+
     } else if (hours > 12 && hours <= 18) {
+
         return 'DAY';
+
     } else return 'NIGHT';
 
 }
