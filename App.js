f99b4b024da9bbbf4714a9ca50ec2795ffec4be6
Hamabama <yastr.dima@gmail.com>
2018-01-25 19:07:14 +0500

    add entry point and tests

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import RootContainer from './src/components/containers/root-container';
+
+export default class App extends React.Component {
+
+  render() {
+    
+    return <RootContainer />;
+  }
+};
