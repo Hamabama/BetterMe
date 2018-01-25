07ace139801b2f6e4a9005c30fece6a8b21d6fbf
Hamabama <yastr.dima@gmail.com>
2018-01-25 18:58:32 +0500

    simplified getCurrentList

--- src/components/helpers/filter.js
+++ src/components/helpers/filter.js
@@ -1,33 +1,35 @@
-import timeHelper from './time';
-
 const filterQuotesByTime = (list, filter) => {
+
     return list.filter(q => q.time === filter);
 }
 
-const getCurrentList = (quotesList) => {
+const getCurrentList = (quotesList, timeName) => {
 
-    const currentTimeName = timeHelper.getCurrentTimeName();
+    switch (timeName) {
 
-    switch (currentTimeName) {
         case 'MORNING':
             return getMorningList(quotesList);
+
         case 'DAY':
             return getDayList(quotesList);
+
         case 'NIGHT':
             return getNightList(quotesList);
     }
-
 }
 
 const getMorningList = (quotesList) => {
+
     return filterQuotesByTime(quotesList, 'MORNING');
 }
 
 const getDayList = (quotesList) => {
+
     return filterQuotesByTime(quotesList, 'DAY');
 }
 
 const getNightList = (quotesList) => {
+
     return filterQuotesByTime(quotesList, 'NIGHT');
 }
 
